09be0d835613b0b97425170ec8bbc09228e30e54
Alison Park <alisonpark@Alisons-MacBook-Pro-2.local>
2022-04-03 16:04:40 -0500

    styling attempt

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+/*
+import Title from './Title.js';
+*/
+import * as React from 'react';
+import Title from './Title.js';
+
 let data = require('./book.json')
 data = data["book-sales"]
 console.log(data)
+
+
+
+
 const Books = () => {
   return (
     data.map(book => { return(
       <Book book={book}/>
     )}))
 }
+
+
 const Book = (props) => {
   return (
     <p>
     {props.book["title"]},
-      <img src={props.book["url"]}/>
+    <br></br>
+      <img src={props.book["url"]} width="100" height="150"/>
+      <br></br>
+      
     {props.book["class"]},
+    <br></br>
     {props.book["seller-name"]},
+    <br></br>
     {props.book["seller-phone"]},
-    {props.book["price"]}</p>
+    <br></br>
+    {props.book["price"]}
+    <br></br>
+   <a href="mailto:mail@address.com?subject=just-a-subject">Contact</a>
+   <br></br>
+   <p></p>
+   </p>
   )
 }
+
 function App() {
   console.log('data');
+
   return (
     <div className="App">
+       
+     <Title/>
         <Books/>
     </div>
   );
 }
+
 export default App;
+
+
---
+++ src/Title.js
@@ -0,0 +1,31 @@
+
+import React from 'react'
+
+function Title() {
+  return (
+    <div>
+      <h1>NU Book</h1>
+    </div>
+  )
+}
+
+
+export default Title
+
+
+
+/*
+function StackScreen() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'My home' }}
+        />
+      </Stack.Navigator>
+    );
+  }
+
+  export default StackScreen
+  */
