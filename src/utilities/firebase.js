8ffa05e13d1c183cc31d7ba50e91140e560fe205
davidsods <dlsotir@gmail.com>
2022-04-03 14:11:14 -0500

    firebase data start

---
+++ src/utilities/firebase.js
@@ -0,0 +1,47 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAYh8iSKpnSR1iqMWHVamJx9J1Cones_Lc",
+  authDomain: "nu-book-79ca9.firebaseapp.com",
+  databaseURL: "https://nu-book-79ca9-default-rtdb.firebaseio.com",
+  projectId: "nu-book-79ca9",
+  storageBucket: "nu-book-79ca9.appspot.com",
+  messagingSenderId: "289119586454",
+  appId: "1:289119586454:web:8d36ab9b9e2fb3b9645ab7",
+  measurementId: "G-W5XDJ29YJB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+  
+    useEffect(() => {
+      const dbRef = ref(database, path);
+      const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+      if (devMode) { console.log(`loading ${path}`); }
+      return onValue(dbRef, (snapshot) => {
+        const val = snapshot.val();
+        if (devMode) { console.log(val); }
+        setData(transform ? transform(val) : val);
+        setLoading(false);
+        setError(null);
+      }, (error) => {
+        setData(null);
+        setLoading(false);
+        setError(error);
+      });
+    }, [path, transform]);
+  
+    return [data, loading, error];
+  };
