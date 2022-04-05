abfa910de2b9b11286def461ad880e7e37c450cf
Alison Park <alisonpark@Alisons-MacBook-Pro-2.local>
2022-04-05 18:03:48 -0500

    input css edits

--- src/App.js
+++ src/App.js
@@ -71,40 +71,49 @@ const Form = (props) => {
       <form id="book-form">
         <label>
           Title:
-          <input type="text" name="title" value='a'/>
+          <input type="text" name="title"/>
         </label>
+        <br></br>
         <label>
           Edition:
-          <input type="text" name="edition" value='a'/>
+          <input type="text" name="edition"/>
         </label>
+        <br></br>
         <label>
           Department:
-          <input type="text" name="department" value='a'/>
+          <input type="text" name="department"/>
         </label>
+        <br></br>
         <label>
           Class number:
-          <input type="text" name="class-number" value='1'/>
+          <input type="text" name="class-number"/>
         </label>
+        <br></br>
         <label>
           Your name:
-          <input type="text" name="seller-name" value='a'/>
+          <input type="text" name="seller-name"/>
         </label>
+        <br></br>
         <label>
           Phone number:
-          <input type="number" name="seller-phone" value='1'/>
+          <input type="number" name="seller-phone"/>
         </label>
+        <br></br>
         <label>
           Price:
-          <input type="number" name="price"  value='1'/>
+          <input type="number" name="price"/>
         </label>
+        <br></br>
         <label>
           Email:
-          <input type="text" name= "email" value='a'/>
+          <input type="text" name= "email" />
         </label>
+        <br></br>
         <label>
           Image:
-          <input type="url" name = "url" value='https://espn.com'/>
+          <input type="url" name = "url" />
         </label>
+        <br></br>
         <button id="submit_button" type="button" 
         value="Submit" onClick={() => {props.postData(props.handleClick)}}>Submit</button>
       </form>
