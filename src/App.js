8e39c79d76083f856b613e8c5cb8bb6670ac8877
Sydney <sydney.rae.smith@gmail.com>
2022-04-10 14:47:00 -0500

    ugly books

--- src/App.js
+++ src/App.js
@@ -36,12 +36,26 @@ This component represents an individual book and its properties.
 */
 const Book = (props) => {
   return (
-    <div class="card" style={{width: '18rem'}}>
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p className="card-text">text</p>
-        
-      </div>  
+    <div class="card" style={{width: '22rem', padding: '25px;'}}>
+      <div class="row no-gutters">
+        <div class="col-md-4">
+        <img src={props.book["url"]} width="100" height="150"/>
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">{props.book["title"]}</h5>
+            <p className="card-text">
+            {props.book["department"]} {props.book["class-number"]}
+            <br></br>
+            {props.book["seller-name"]}
+            <br></br>
+            ${props.book["price"]}
+            <br></br>
+            <a href={"mailto:" + props.book["email"] + "?subject=just-a-subject"}>Contact</a>
+            </p>
+          </div> 
+        </div>
+      </div> 
     </div>
 
     // <div className = "book">
