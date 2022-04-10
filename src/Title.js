71c62df3aefdf713182d0bd8c1b5168e03515d5d
Sm1te <liyijiannice@outlook.com>
2022-04-10 13:21:33 -0500

    Refined with header by MUI

--- src/Title.js
+++ src/Title.js
@@ -1,19 +1,37 @@
 
-import React from 'react'
+// import React from 'react'
 
-function Title() {
+// function Title() {
+//   return (
+//     <div id="navbar">
+//       <h1>NU Book</h1>
+//     </div>
+//   )
+// }
+
+
+// export default Title
+
+
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+const Title = () => {
   return (
-    <div id="navbar">
-      <h1>NU Book</h1>
+    <div>
+      <AppBar position="fixed">
+        <Toolbar color="purple">
+          <Typography variant="h6">NU Book</Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
-  )
-}
-
-
-export default Title
-
-
-
+  );
+};
+export default Title;
 /*
 function StackScreen() {
     return (
