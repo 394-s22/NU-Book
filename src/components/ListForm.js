e06f75e30eb55a941392206f213d708e46489f0e
Lingyue <31970601+lingyue328@users.noreply.github.com>
2022-04-16 14:39:45 -0500

    update css

--- src/components/ListForm.js
+++ src/components/ListForm.js
@@ -160,14 +160,10 @@ export const Form = (props) => {
     else {
       return (
         <div className="listBook">
-           <br></br>
-           <br></br>
-           <br></br>
-           <br></br>
-           {/* !user ? ()=> alert("you must sign in first to list a book"): */}
+          {/* !user ? ()=> alert("you must sign in first to list a book"): */}
           <Button variant="primary" size="sm" onClick={props.handleClick}>List a Book</Button>
           {/* <button type = "button" className = "btn-primary" onClick={ props.handleClick}>List a Book</button> */}
-          <Button variant="secondary" size="sm" onClick={props.handleClick}>Search for a Book</Button>
+          <Button variant="secondary" size="sm" onClick={props.handleClick}>Search Books</Button>
           {/* <button type = "button" className = "btn-secondary" onClick={ props.handleClickSearch}>Search for a Book</button>       */}
         </div>
       )
