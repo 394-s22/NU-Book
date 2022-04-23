8f4613e54c2ce290d1c013c7c6d13d98e9dba039
sosalover <91343823+sosalover@users.noreply.github.com>
2022-04-23 14:56:32 -0500

    attempting to get URLs

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,48 +1,54 @@
-import { useEffect, useState } from 'react';
-import { ref, listAll } from "firebase/storage";
-import { storage } from "../utilities/firebase";
 
+import { getDownloadURL, listAll, ref } from 'firebase/storage';
+import {useEffect, useState} from 'react'
+import {storage} from '../utilities/firebase.js'
+  
 /* 
 This component represents an individual book and its properties.
 */
 const Book = (props) => {
-  const [image, setImage] = useState(""); // should image be a string?
-  const imageListRef = ref(storage, "images/") // change to file url
-  // based on the props, determine what the url will be and set it
-  // so in the other file, when we upload an image, set the url appropriately accordingly
-  useEffect(() => {
-    listAll(imageListRef).then((response) => {
-      
+  const [imageList, setImageList] = useState([]);
+  const listFormTitle= props.book["title"] ;
+  const listFormName= props.book["seller-name"]
+  const im_identifier=listFormTitle + listFormName;
+  //const imageListRef = ref(storage, `images/${im_identifier}` + ".jfif");
+  const imageListRef = ref(storage, `images/dababy` + ".jfif");
 
-    })
-  }, []);
-
-  return (
-    <div class="card" style={{width: '22rem', padding: '15px'}}>
-      <div class="row no-gutters">
-        <div class="col-md-4">
-        {/* change image here */}
-        <img src={props.book["url"]} alt='book-preview' width="100" height="150"/>
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">{props.book["title"]}</h5>
-            <p className="card-text">
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">{props.book["department"]} {props.book["class-number"]}</li>
-              <li class="list-group-item">{props.book["seller-name"]}</li>
-              <li class="list-group-item">${props.book["price"]}</li>
-              <li class="list-group-item">{props.book["quality"]}</li>
-            </ul>
-
-            {/* <a href={"mailto:" + props.book["email"] + "?subject=just-a-subject"}>Contact</a> */}
-            <a class="btn btn-outline-success btn-sm" href={"mailto:" + props.book["email"] + "?subject=just-a-subject"} role="button">Contact</a>
-            </p>
-          </div> 
-        </div>
-      </div> 
-    </div>
-  )
+  console.log(imageListRef);
+  const imagePath = imageListRef._location.path_;
+  console.log(imagePath);
+  let imageURL;
+  getDownloadURL(ref(storage, imagePath)).then((url) => {
+    imageURL = url;
+    console.log(url);
+    return (
+      <div class="card" style={{width: '22rem', padding: '15px'}}>
+        <div class="row no-gutters">
+          <div class="col-md-4">
+          {/* change image here */}
+          <img src={imageURL} alt='book-preview' width="100" height="150"/>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">{props.book["title"]}</h5>
+              <p className="card-text">
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">{props.book["department"]} {props.book["class-number"]}</li>
+                <li class="list-group-item">{props.book["seller-name"]}</li>
+                <li class="list-group-item">${props.book["price"]}</li>
+                <li class="list-group-item">{props.book["quality"]}</li>
+              </ul>
+  
+              {/* <a href={"mailto:" + props.book["email"] + "?subject=just-a-subject"}>Contact</a> */}
+              <a class="btn btn-outline-success btn-sm" href={"mailto:" + props.book["email"] + "?subject=just-a-subject"} role="button">Contact</a>
+              </p>
+            </div> 
+          </div>
+        </div> 
+      </div>
+    )
+  });
+  
 }
 
 export default Book;
