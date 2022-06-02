f325c562243f5ac6b8d7fd0872b247cf0505b855
Lingyue <31970601+lingyue328@users.noreply.github.com>
2022-06-02 17:04:04 -0500

    pass initial test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('check for text NU Book', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/book/i);
+  expect(title).toBeInTheDocument();
 });
