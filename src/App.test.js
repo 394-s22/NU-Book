09e451239216b8a9a5921acad2a4204bb428cf36
Lingyue <31970601+lingyue328@users.noreply.github.com>
2022-06-02 17:05:18 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('check for text NU Book', () => {
+test('check for text book', () => {
   render(<App />);
   const title = screen.getByText(/book/i);
   expect(title).toBeInTheDocument();
