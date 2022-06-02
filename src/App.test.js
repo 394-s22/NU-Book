d2ccdd81978af54ec08b5c40b6a1268af891b43c
Lingyue <31970601+lingyue328@users.noreply.github.com>
2022-06-02 18:58:54 -0500

    Mock user authentication

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('check for text book', () => {
+test('check for text when loading book', () => {
   render(<App />);
-  const title = screen.getByText(/book/i);
+  const title = screen.getByText(/loading the books/i);
   expect(title).toBeInTheDocument();
 });
