42eb60affa80a88d2864e756c85ee7c370f505c3
Railgun707 <73008040+Railgun707@users.noreply.github.com>
2022-06-07 17:43:57 -0500

    search book page render test

---
+++ src/SearchBook.test.js
@@ -0,0 +1,29 @@
+//Author: David Cheung
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import { useData, useUserState }from './utilities/firebase.js';
+
+
+import userEvent from '@testing-library/user-event';
+jest.mock('./utilities/firebase.js', () => {
+  const originalB = jest.requireActual('./utilities/firebase.js');
+  const partialMockedB = Object.keys(originalB).reduce((pre, methodName) => {
+    pre[methodName] = jest.fn();
+    return pre;
+  }, {});
+  return {
+    ...partialMockedB,
+    useData: originalB.useData,
+    storage: originalB.storage // mock all methods of b except method3
+  };
+});
+
+test('Search page loads', async () => {
+    useUserState.mockReturnValue([{ displayName: "Test User" }, false, null]);
+    render(<App />);
+    await new Promise((r) => setTimeout(r, 500));
+    const sbutt = screen.getByText(/Search Book/i);
+    userEvent.click(sbutt);
+    const btn = screen.getByText(/Submit/i);
+    expect(btn).toBeInTheDocument();
+  });
